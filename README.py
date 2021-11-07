694172aa165aa3f58f166e95aef66e91fb8eddca
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-11-07 23:37:49 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,8 +1,23 @@
+# LOVELY Boy
+
+LOVELY IS Boy
+
+#DEPLOY 
+
+CLICK ON BELOW IMAGE TO CONTACT ME
+
+[![DEPLOY](https://telegra.ph/file/002151a1eb0040d0b7b90.jpg)](https://heroku.com/deploy?template=https://github.com/attitudeking1/LOVELYX)
+
+Keep supporting us 😊
 
 
 
 
 
-[![LOVELY BOY 💘](https://telegra.ph/file/66817e4d190d8cf2e437a.jpg)](https://t.me/Horimaya)
 
-# Keep supporting us 💝
+
+
+
+
+
+
